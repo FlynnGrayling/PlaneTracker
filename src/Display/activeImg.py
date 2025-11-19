a37832d472a4486e6de5d4e4d690aeb4e2c23eb1
Flynn Grayling <fgrayling@outlook.com>
2025-11-19 20:08:27 +1000

    fixed null iteration over states

--- src/Display/activeImg.py
+++ src/Display/activeImg.py
@@ -7,7 +7,6 @@ def setImage(flights, displayCount):
     
     if len(flights) > 0 and displayCount < 10:
         generateMap(flights)
-        generateHistory(flights)
         displayCount = displayCount + 1
     else:
         generateHistory(flights)
--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ import time
 
 def main():
     # bounding box properties (fixed coordinates)
-    lamin = -27.580000  # lower latitude
+    lamin = -27.530000  # lower latitude
     lamax = -27.420000 # higher latitude
     lomin = 153.000000  # lower longitude
     lomax = 153.170000  # higher longitude
@@ -50,18 +50,28 @@ def main():
         #call API
         data = get_states(params=params, username=username, password=password)
 
+        
+
         #if response parse wanted data to Flights list
         if data:
             print("Successful Get")
             print("API Response (JSON):")
             print(data)
+            states = data.get('states')
             #callsigns = [state[1].strip() for state in data['states'] if state[1].strip() != "NoCallsign"]
-            flights = [
-                [state[1].strip(), state[6], state[5], 0, 0, state[2].strip(), state[3]]
-                for state in data['states']
-                if state[1].strip() != "NoCallsign" and state[6] != "NoLat" and state[5] != "NoLon"
-            ]
-            print(flights)
+            #check if data has returned some states, if not show default image
+            if states and isinstance(states, list) and len(states) > 0:
+                flights = [
+                    [state[1].strip(), state[6], state[5], 0, 0, state[2].strip(), state[3]]
+                    for state in states
+                    if state[1].strip() != "NoCallsign" 
+                    and state[6] != "NoLat" 
+                    and state[5] != "NoLon"
+                ]
+                print(flights)
+            else:
+                #if states is empty set default display
+                displayCount = setImage(flights, displayCount)
         
         for plane in flights:
             #convert lon/lat to pixelcoords
