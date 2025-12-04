e37944d2f6eea45d1a5058e7498743fc09efb49a
Flynn Grayling <fgrayling@outlook.com>
2025-12-04 22:20:04 +1000

    Update main comments

--- src/main.py
+++ src/main.py
@@ -7,17 +7,13 @@ from staticmap import StaticMap, CircleMarker
 import time
 
 def main():
-    # bounding box properties (fixed coordinates)
+    #bounding box properties (fixed coordinates)
     lamin = -27.540000  # lower latitude
     lamax = -27.410000 # higher latitude
     lomin = 152.990000  # lower longitude
     lomax = 153.250000  # higher longitude
 
-    #middle coord values for map title
-    lamid = lamin + lamax / 2 #-27.500000
-    lomid = lomin + lomax / 2 #153.085000
-
-    # define query parameters as dictionary
+    #query parameters
     params = {
         "time": 0,
         "icao24": "",
@@ -30,7 +26,7 @@ def main():
     #Map darkmode, toggle for white or black map, darkmode is on by default
     darkMode = True
 
-    storedFlights = []  # Initialize before loop
+    storedFlights = [] 
     displayCount = 0
 
     while True:
@@ -40,16 +36,12 @@ def main():
         #call API
         data = get_states(params=params)
 
-        
-
         #if response parse wanted data to Flights list
         if data:
             print("Successful Get")
             print("API Response (JSON):")
             print(data)
             states = data.get('states')
-            #callsigns = [state[1].strip() for state in data['states'] if state[1].strip() != "NoCallsign"]
-            #check if data has returned some states, if not show default image
             if states and isinstance(states, list) and len(states) > 0:
                 flights = [
                     [state[1].strip(), state[6], state[5], 0, 0, state[2].strip(), state[3]]
@@ -71,6 +63,7 @@ def main():
         current_callsigns = {plane[0]: (plane[1], plane[2]) for plane in flights}
         stored_callsigns = {plane[0]: (plane[1], plane[2]) for plane in storedFlights}
         
+        #Update display only if current callsigns changes 
         if current_callsigns != stored_callsigns:
             print("Flight data changed - updating display")
             displayCount = setImage(flights, displayCount, darkMode)
@@ -79,6 +72,7 @@ def main():
 
         storedFlights = flights
 
+        #screen refresh set to 1 min
         time.sleep(60)
 
 if __name__ == "__main__":
